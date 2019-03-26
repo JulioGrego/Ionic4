6c8e47d6c581af29c5f614b2deb4a3deb5e25f43
unknown <jcesargregorio@gmail.com>
2019-03-26 16:00:57 -0300

    Inserindo no github

---
+++ src/app/pages/todo-details/todo-details.page.ts
@@ -0,0 +1,63 @@
+import { Todo, TodoService } from './../../services/todo.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, LoadingController } from '@ionic/angular';
+ 
+@Component({
+  selector: 'app-todo-details',
+  templateUrl: './todo-details.page.html',
+  styleUrls: ['./todo-details.page.scss'],
+})
+export class TodoDetailsPage implements OnInit {
+ 
+  todo: Todo = {
+    task: '', //Produto
+    createdAt: new Date().getTime(),
+    priority: null, //2
+    preco: null    //10 
+  };
+ 
+  todoId = null;
+ 
+  constructor(private route: ActivatedRoute, private nav: NavController, private todoService: TodoService, private loadingController: LoadingController) { }
+ 
+  ngOnInit() {
+    this.todoId = this.route.snapshot.params['id'];
+    if (this.todoId)  {
+      this.loadTodo();
+    }
+  }
+ 
+  async loadTodo() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando..'
+    });
+    await loading.present();
+ 
+    this.todoService.getTodo(this.todoId).subscribe(res => {
+      loading.dismiss();
+      this.todo = res;
+    });
+  }
+ 
+  async saveTodo() {
+ 
+    const loading = await this.loadingController.create({
+      message: 'Salvando..'
+    });
+    await loading.present();
+ 
+    if (this.todoId) {
+      this.todoService.updateTodo(this.todo, this.todoId).then(() => {
+        loading.dismiss();
+//this.nav.goBack('home');
+      });
+    } else {
+      this.todoService.addTodo(this.todo).then(() => {
+        loading.dismiss();
+//this.nav.goBack('home');
+      });
+    }
+  }
+ 
+}
